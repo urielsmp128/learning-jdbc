b8cdabf21fdfcec96208f4f6e81a580b1bab500e
urielsmp128 <urielsmp128@outlook.com>
2024-05-20 17:08:51 -0600

    Initializating project

---
+++ learning-jfbc/src/main/java/com/urielm/lil/jdbc/JDBCExecutor.java
@@ -0,0 +1,26 @@
+package com.urielm.lil.jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCExecutor {
+
+    public static void main(String[] args) {
+
+        DatabaseConnectionManager databaseConnectionManager = new DatabaseConnectionManager("localhost", "hplussport", "postgres", "password");
+
+        try{
+            Connection connection = databaseConnectionManager.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet =  statement.executeQuery("SELECT COUNT(*) FROM CUSTOMER");
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt(1));
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ learning-jfbc/src/main/java/com/urielm/lil/jdbc/util/DataTransferObject.java
@@ -0,0 +1,6 @@
+package com.urielm.lil.jdbc.util;
+
+public interface DataTransferObject {
+
+    long getId();
+}
